96e8fa24cdba1a2864f5c2b8e3a3c4f9bc759b38
Tseng722 <cindy2270@gmail.com>
2024-03-22 18:13:25 +0800

    save summary graph to html#2

--- app/management/commands/update_summary.py
+++ app/management/commands/update_summary.py
@@ -24,6 +24,16 @@ class Command(BaseCommand):
         df = read_frame(queryset)
         df_a = df.loc[df['class_type']=='A']
         df_a = df_a.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        
+        # df_a['hla_type'] = df_a.apply(lambda x: 'Other' if x['patients'] <= 1 else x['hla_type'], axis=1)
+        # df_a.reset_index(drop=True, inplace=True)
+        # df_other = df_a.loc[df_a['hla_type']=='Other']
+        # df_other = df_other.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        # df_a = df_a.loc[df_a['hla_type']!='Other']
+        # df_a = pd.concat([df_a,df_other])
+        df_a.to_csv('/work1791/cindy2270/web/web_v1/webV1/static/file/hla-a.csv',index = False)
+        
+
         fig_hla_a = px.bar(df_a, x="hla_type", y="patients", color="tumor_type", title="HLA-A type")
         graph_html_hla_a = plot(fig_hla_a, output_type='div')
         with open(OUT_HTML_DIR+'/graph_hla_a.html', 'w') as file:
@@ -31,6 +41,13 @@ class Command(BaseCommand):
         print('hla_a')
         df_b = df.loc[df['class_type']=='B']
         df_b = df_b.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        # df_b['hla_type'] = df_b.apply(lambda x: 'Other' if x['patients'] <= 4 else x['hla_type'], axis=1)
+        # df_b.reset_index(drop=True, inplace=True)
+        # df_other = df_b.loc[df_b['hla_type']=='Other']
+        # df_other = df_other.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        # df_b = df_b.loc[df_b['hla_type']!='Other']
+        # df_b = pd.concat([df_b,df_other])
+
         df_b.to_csv('/work1791/cindy2270/web/web_v1/webV1/static/file/hla-b.csv',index = False)
         fig_hla_b = px.bar(df_b, x="hla_type", y="patients", color="tumor_type", title="HLA-B type")
         graph_html_hla_b = plot(fig_hla_b, output_type='div')
@@ -40,6 +57,14 @@ class Command(BaseCommand):
 
         df_c = df.loc[df['class_type']=='C']
         df_c = df_c.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        
+        # df_c['hla_type'] = df_c.apply(lambda x: 'Other' if x['patients'] <= 4 else x['hla_type'], axis=1)
+        # df_c.reset_index(drop=True, inplace=True)
+        # df_other = df_c.loc[df_c['hla_type']=='Other']
+        # df_other = df_other.groupby(["hla_type","tumor_type"]).size().reset_index(name="patients").sort_values(by='patients', ascending=False)
+        # df_c = df_c.loc[df_c['hla_type']!='Other']
+        # df_c = pd.concat([df_c,df_other])
+
         df_c.to_csv('/work1791/cindy2270/web/web_v1/webV1/static/file/hla-c.csv',index = False)
         fig_hla_c = px.bar(df_c, x="hla_type", y="patients", color="tumor_type", title="HLA-C type")
         graph_html_hla_c = plot(fig_hla_c, output_type='div')
